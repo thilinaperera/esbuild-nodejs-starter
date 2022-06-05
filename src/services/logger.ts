9704963fb5f61f1d7f89fb8c490e6947712805ec
Thilina Perera <thilina@thilina.lk>
2022-06-05 21:56:47 +0100

    Code refactor :recycle:

--- src/services/logger.ts
+++ src/services/logger.ts
@@ -1,17 +1,15 @@
 import pino from "pino";
 import { isProduction } from "../utils/misc";
 import { Logger } from "../interfaces/Logger";
-export const logger: Logger = (() => {
-  const configs = isProduction
-    ? {}
-    : {
-        transport: {
-          target: "pino-pretty",
-          options: {
-            colorize: true,
-          },
+
+const configs = isProduction
+  ? {}
+  : {
+      transport: {
+        target: "pino-pretty",
+        options: {
+          colorize: true,
         },
-      };
-  const pinoLogger: Logger = pino(configs);
-  return pinoLogger;
-})();
+      },
+    };
+export const logger: Logger = pino(configs);
